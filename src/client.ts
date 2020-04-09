c6fd113199936adcc7ff60e63b1db1b1597d805d
ikhsanalatsary <skull.saders18@gmail.com>
2020-04-09 22:01:57 +0700

    :ok_hand: certs gen

--- src/client.ts
+++ src/client.ts
@@ -1,15 +1,32 @@
 import { HelloClient } from "../generated/hello_grpc_pb";
 import { credentials } from "grpc";
 import { HelloRequest } from "../generated/hello_pb";
+import { readFileSync } from 'fs';
 
 function main() {
     const host = "localhost";
     const port = ":6000";
-    let client = new HelloClient(host + port, credentials.createInsecure());
+    const cacert = readFileSync('certs/ca.crt'),
+        cert = readFileSync('certs/client.crt'),
+        key = readFileSync('certs/client.key'),
+        kvpair = {
+            'private_key': key,
+            'cert_chain': cert
+        },
+        options = {
+        'grpc.ssl_target_name_override' : 'example.com',
+        'grpc.default_authority': 'example.com'
+        };
+    const creds = credentials.createSsl(cacert, key, cert);
+    let client = new HelloClient(host + port, creds);
     let req = new HelloRequest();
     req.setFirstName('John'), req.setLastName('Wick');
 
     client.greeting(req, function (err, res) {
+        if (err) {
+            console.error(err);
+            return
+        }
         console.log('Greeting: %s', res.getMessage());
     })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import * as grpc from "grpc";
 import { HelloRequest, HelloResponse } from "../generated/hello_pb";
 import { HelloService } from "../generated/hello_grpc_pb";
+import { readFileSync } from 'fs';
 
-const host = "localhost";
+const host = "0.0.0.0";
 const port = ":6000";
 const server = new grpc.Server();
+const cacert = readFileSync('certs/ca.crt'),
+    cert = readFileSync('certs/server.crt'),
+    key = readFileSync('certs/server.key'),
+    kvpair = {
+        'private_key': key,
+        'cert_chain': cert
+    };
+
+const creds = grpc.ServerCredentials.createSsl(cacert, [kvpair], true);
 
 function greeting(call: grpc.ServerUnaryCall<HelloRequest>, callback: grpc.sendUnaryData<HelloResponse>) {
     let req = call.request;
@@ -17,6 +27,6 @@ function greeting(call: grpc.ServerUnaryCall<HelloRequest>, callback: grpc.sendU
 }
 
 server.addService(HelloService, { greeting });
-server.bind(host + port, grpc.ServerCredentials.createInsecure());
+server.bind(host + port, creds);
 server.start();
 console.log(`GRPC is now running on ${host + port}`);
